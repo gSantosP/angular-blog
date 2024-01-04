477c6ca8b2a993e040f3ddbd8882e1b0d9763e35
Gabriel <gabriel7132@protonmail.com>
2024-01-04 08:58:54 -0300

    First Commit.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,32 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Clean Code Book!",
+    "description": "This book is a must for any developer, software engineer, project manager, team lead, or systems analyst with an interest in producing better code.",
+    "photoCover":"https://i.imgur.com/EHex9XC.png"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "How programming helps you think?",
+    "description": "It hones problem-solving and analysis skills, such as finding errors and thinking logically.",
+    "photoCover":"https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/07/computer-coding.jpg"
+  },
+  {
+    "id":"3",
+    "title": "How to make money as a software engineer?",
+    "description": "1- Mobile app development. 2- E-commerce website development. 3- Online coding courses  4- Affiliate marketing for tech products.",
+    "photoCover":"https://imageio.forbes.com/blogs-images/ofx/files/2018/09/OFX3-iStock-492595743-1200x800.jpg?height=474&width=711&fit=bounds"
+  },
+  {
+    "id":"4",
+    "title": "Reasons to buy an ergonomic chair.",
+    "description": "Ergonomic chairs are fully adjustable to align with your height and workstation, ensuring you remain in the correct sitting posture.",
+    "photoCover":"https://i5.walmartimages.com/seo/Mainstays-Ergonomic-Mesh-Back-Task-Office-Chair-with-Flip-up-Arms-Black-Fabric-275-lb_584c4cc5-fc4d-46d3-9a23-5a3fb5bb134e.6c681a1d24f26e9c017a3a011877e2d8.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF"
+  },
+  {
+    "id":"5",
+    "title": "How to protect your data online?",
+    "description": "1- Create strong passwords. 2- Don't overshare on social media. 3- Use free wi-fi with caution. 4- Watch out for links and attachments.",
+    "photoCover":"https://fotografias.lasexta.com/clipping/cmsimages01/2016/06/09/28967CDC-C7A3-40FA-B727-E61AD470C0F2/98.jpg?crop=3515,1978,x0,y251&width=1900&height=1069&optimize=high&format=webply"
   }
 ]
